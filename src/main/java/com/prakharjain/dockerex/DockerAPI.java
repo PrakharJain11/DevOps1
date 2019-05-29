e7e48cb7f1599d2b5544ab1ec7b1aba23ec2e663
prakharshahbajaj@gmail.com <prakhar.jain@clarivate.com>
2019-05-29 21:24:12 +0530

    Initial Commit

---
+++ src/main/java/com/prakharjain/dockerex/DockerAPI.java
@@ -0,0 +1,12 @@
+package com.prakharjain.dockerex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAPI {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DockerAPI.class, args);
+	}
+}
