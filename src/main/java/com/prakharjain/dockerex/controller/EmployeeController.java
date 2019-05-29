151aaf36f0cd85ab002fad3b1ad5edd59bbab818
prakharshahbajaj@gmail.com <prakhar.jain@clarivate.com>
2019-05-30 00:35:17 +0530

    test commit for drone

--- src/main/java/com/prakharjain/dockerex/controller/EmployeeController.java
+++ src/main/java/com/prakharjain/dockerex/controller/EmployeeController.java
@@ -13,7 +13,7 @@ public class EmployeeController {
 	@RequestMapping(value="/hi", method=RequestMethod.GET)
 	public String sayHi()
 	{
-		return "Hi Dude";
+		return "Hi Dude1";
 	}
 	
     /*//Happy path, an employee is returned as response
